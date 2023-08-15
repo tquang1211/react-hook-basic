cd385921c469db98af02180c541acbc27b4281d2
Quang Tran <tquang1211@gmail.com>
2023-08-15 16:30:59 +0700

    Multiple Components với React Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
 
 // 1 component: template + logic
 // JSX là 1 đoạn code html trong 1 file js
@@ -7,15 +8,29 @@ import './App.css';
 
 //function App() {
 const App = () => {
+  let name = 'Demo';
+  let number = 2021;
+  let obj = { name: 'DeMo', channel: 'Demo Channel' };
+  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=10';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          Hello world with React hook.
-        </h2>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Nav />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>
+            Hello world with React hook {name}.
+          </h2>
+          <p>{JSON.stringify(obj)}</p>
+          <a href={link} target='_blank'>Visit my channel</a>
+        </header>
+      </div>
+
+      <div>
+
+      </div>
+    </>
   );
 }
 
