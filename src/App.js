0e2697009cd6d903bef378d49e21b29ebb8ee0e7
Quang Tran <tquang1211@gmail.com>
2023-08-15 10:53:53 +0700

    hạ version react 18 xuống 17

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+        <h2>
           Hello world with React hook.
-        </h1>
+        </h2>
       </header>
     </div>
   );
